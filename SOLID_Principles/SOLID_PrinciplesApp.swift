2df161194f74d6836e2dc7374ea76fc70e268dea
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-18 16:07:56 +0200

    Initial Commit

---
+++ SOLID_Principles/SOLID_PrinciplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLID_PrinciplesApp.swift
+//  SOLID_Principles
+//
+//  Created by Eric on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SOLID_PrinciplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
